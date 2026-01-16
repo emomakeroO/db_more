c79ddfd5ac95889cffb3fe8284d0f18b5beedc01
emomakeroO <128972902+emomakeroO@users.noreply.github.com>
2026-01-16 13:46:46 +0800

    Update our_resnet_db.py

--- models/our_resnet_db.py
+++ models/our_resnet_db.py
@@ -1,240 +1,206 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.models.resnet import Bottleneck, ResNet
 
-class ResNet_ImageNet(ResNet):
-    def __init__(self, block, num_blocks, num_classes=1000, return_features=False):
-        super(ResNet_ImageNet, self).__init__(block, num_blocks, num_classes=num_classes)
-        self.return_features = return_features
-        self.penultimate_layer_dim = self.fc.weight.shape[1]
-        print('self.penultimate_layer_dim:', self.penultimate_layer_dim)
-        # self.aux_linear = nn.Linear(self.penultimate_layer_dim, num_classes)
-        self.projection = nn.Sequential(nn.Linear(self.penultimate_layer_dim, self.penultimate_layer_dim), nn.ReLU(), nn.Linear(self.penultimate_layer_dim, 128))
-        self.linear_classfier0 = nn.Linear(2048, num_classes)
-        self.linear_classfier1 = nn.Linear(2048, num_classes)
-        self.linear_classfier2 = nn.Linear(2048, num_classes)
-    def forward_features(self, x):
-        c1 = self.maxpool(self.relu(self.bn1(self.conv1(x))))
-        h1 = self.layer1(c1) # (64,32,32)
-        h2 = self.layer2(h1) # (128,16,16)
-        h3 = self.layer3(h2) # (256,8,8)
-        h4 = self.layer4(h3) # (512,4,4)
-        p4 = self.avgpool(h4) # (512,1,1)
-        p4 = torch.flatten(p4, 1) # (512)
-        return p4
+class BasicBlock(nn.Module):
+    expansion = 1
+    def __init__(self, in_planes, mid_planes, out_planes, norm, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, mid_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = norm(mid_planes)
+        self.conv2 = nn.Conv2d(mid_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = norm(out_planes)
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != out_planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False),
+                norm(out_planes)
+            )
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
-    def forward_classifier(self, p4):
-        logits0 = self.linear_classfier0(p4) # (10)
-        logits1 = self.linear_classfier1(p4)
-        logits2 = self.linear_classfier2(p4)
-        return logits0, logits1, logits2
-
-    # def forward_aux_classifier(self, p4):
-    #     logits = self.aux_linear(p4) # (10)
-    #     return logits
+class Decoder(nn.Module):
+    def __init__(self, input_dim=512):
+        super(Decoder, self).__init__()
+        self.deconv1 = nn.ConvTranspose2d(input_dim, 256, 4, stride=2, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(256, 128, 4, stride=2, padding=1)
+        self.deconv3 = nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1)
+        self.deconv4 = nn.ConvTranspose2d(64, 3, 3, stride=1, padding=1)
 
     def forward(self, x):
-        p4 = self.forward_features(x)
-        logits0, logits1, logits2 = self.forward_classifier(p4)
+        x = F.relu(self.deconv1(x))
+        x = F.relu(self.deconv2(x))
+        x = F.relu(self.deconv3(x))
+        x = self.deconv4(x)
+        return x
 
-        return logits0, logits1, logits2
+
+class DynamicBiasNetwork(nn.Module):
+    """
+    """
+    def __init__(self, num_classes, hidden_dim=64):
+        super(DynamicBiasNetwork, self).__init__()
+        self.num_classes = num_classes
+        self.class_embed = nn.Embedding(num_classes, hidden_dim)
+
+        self.bias_mlp = nn.Sequential(
+            nn.Linear(hidden_dim, hidden_dim // 2),
+            nn.ReLU(),
+            nn.Linear(hidden_dim // 2, 3)  # 输出3个专家的 Bias
+        )
+        nn.init.xavier_uniform_(self.class_embed.weight)
+
+    def forward(self, class_idx):
+        class_emb = self.class_embed(class_idx)
+        bias = self.bias_mlp(class_emb)  # [B, 3]
+        return bias
 
 
-def ResNet50(num_classes=1000, return_features=False):
-    return ResNet_ImageNet(Bottleneck, [3, 4, 6, 3], num_classes=num_classes, return_features=return_features)
+class ResNetAE(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10, pooling='avgpool',
+                 norm=nn.BatchNorm2d, recon_weight=1.0, class_freq=None, sparse_weight=0.01,
+                 dcb_weight=0.1, alpha=1.0, beta=1.0, gamma=1.0):
+        super(ResNetAE, self).__init__()
 
-if __name__ == '__main__':
-    from thop import profile
-    net = ResNet50(num_classes=10)
-    x = torch.randn(1,3,224,224)
-    flops, params = profile(net, inputs=(x, ))
-    y = net(x)
-    print(y)
-    print('GFLOPS: %.4f, #params: %.4fM' % (flops/1e9, params/1e6)) # GFLOPS: 4.1095, #params: 23.5285M
+        self.in_planes = 64
+        self.num_classes = num_classes
+        self.recon_weight = recon_weight
+        self.sparse_weight = sparse_weight
+        self.dcb_weight = dcb_weight
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
 
-    bn_parameter_number, fc_parameter_number, all_parameter_number = 0, 0, 0
-    for name, p in net.named_parameters():
-        if 'bn' in name:
-            bn_parameter_number += p.numel()
-        if 'fc' in name:
-            fc_parameter_number += p.numel()
-        if 'projection' not in name:
-            all_parameter_number += p.numel()
+        if class_freq is not None:
+            probs = torch.tensor(class_freq, dtype=torch.float32)
+            self.class_probs = probs / probs.sum()
+        else:
+            self.class_probs = None
 
-    all_size = all_parameter_number * 4 /1e6 
-    bn_size = bn_parameter_number * 4 /1e6 
-    fc_size = fc_parameter_number * 4 /1e6 
+        if pooling == 'avgpool':
+            self.pooling = nn.AvgPool2d(4)
+        elif pooling == 'maxpool':
+            self.pooling = nn.MaxPool2d(4)
 
-    print('all_size: %s MB' % (all_size), 2*all_size)
-    print('bn_size: %s MB' % (all_size+bn_size), bn_size)
-    print('fc_size: %s MB' % (all_size+fc_size), fc_size)
-    print('both_size: %s MB' % (all_size+bn_size+fc_size))
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = norm(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1, norm=norm)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2, norm=norm)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, norm=norm)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, norm=norm)
+        self.feat_dim = 512 * block.expansion
+        self.classifier = nn.Linear(self.feat_dim, num_classes)
+                     
+        self.decoder0 = Decoder(input_dim=self.feat_dim)
+        self.decoder1 = Decoder(input_dim=self.feat_dim)
+        self.decoder2 = Decoder(input_dim=self.feat_dim)
 
-'''
-module.conv1.weight         
-module.bn1.weight           
-module.bn1.bias             
-module.layer1.0.conv1.weight
-module.layer1.0.bn1.weight  
-module.layer1.0.bn1.bias    
-module.layer1.0.conv2.weight
-module.layer1.0.bn2.weight  
-module.layer1.0.bn2.bias    
-module.layer1.0.conv3.weight
-module.layer1.0.bn3.weight  
-module.layer1.0.bn3.bias                                                                                                                                  
-module.layer1.0.downsample.0.weight
-module.layer1.0.downsample.1.weight
-module.layer1.0.downsample.1.bias
-module.layer1.1.conv1.weight
-module.layer1.1.bn1.weight  
-module.layer1.1.bn1.bias    
-module.layer1.1.conv2.weight
-module.layer1.1.bn2.weight  
-module.layer1.1.bn2.bias           
-module.layer1.1.conv3.weight       
-module.layer1.1.bn3.weight  
-module.layer1.1.bn3.bias    
-module.layer1.2.conv1.weight
-module.layer1.2.bn1.weight
-module.layer1.2.bn1.bias    
-module.layer1.2.conv2.weight
-module.layer1.2.bn2.weight
-module.layer1.2.bn2.bias
-module.layer1.2.conv3.weight
-module.layer1.2.bn3.weight
-module.layer1.2.bn3.bias
-module.layer2.0.conv1.weight       
-module.layer2.0.bn1.weight       
-module.layer2.0.bn1.bias    
-module.layer2.0.conv2.weight
-module.layer2.0.bn2.weight  
-module.layer2.0.bn2.bias    
-module.layer2.0.conv3.weight
-module.layer2.0.bn3.weight         
-module.layer2.0.bn3.bias           
-module.layer2.0.downsample.0.weight
-module.layer2.0.downsample.1.weight
-module.layer2.0.downsample.1.bias
-module.layer2.1.conv1.weight
-module.layer2.1.bn1.weight  
-module.layer2.1.bn1.bias    
-module.layer2.1.conv2.weight
-module.layer2.1.bn2.weight  
-module.layer2.1.bn2.bias    
-module.layer2.1.conv3.weight
-module.layer2.1.bn3.weight  
-module.layer2.1.bn3.bias    
-module.layer2.2.conv1.weight
-module.layer2.2.bn1.weight  
-module.layer2.2.bn1.bias    
-module.layer2.2.conv2.weight
-module.layer2.2.bn2.weight  
-module.layer2.2.bn2.bias    
-module.layer2.2.conv3.weight                                                                                                                              
-module.layer2.2.bn3.weight
-module.layer2.2.bn3.bias    
-module.layer2.3.conv1.weight
-module.layer2.3.bn1.weight
-module.layer2.3.bn1.bias    
-module.layer2.3.conv2.weight
-module.layer2.3.bn2.weight
-module.layer2.3.bn2.bias    
-module.layer2.3.conv3.weight       
-module.layer2.3.bn3.weight         
-module.layer2.3.bn3.bias
-module.layer3.0.conv1.weight
-module.layer3.0.bn1.weight
-module.layer3.0.bn1.bias    
-module.layer3.0.conv2.weight
-module.layer3.0.bn2.weight
-module.layer3.0.bn2.bias    
-module.layer3.0.conv3.weight
-module.layer3.0.bn3.weight
-module.layer3.0.bn3.bias    
-module.layer3.0.downsample.0.weight
-module.layer3.0.downsample.1.weight
-module.layer3.0.downsample.1.bias
-module.layer3.1.conv1.weight
-module.layer3.1.bn1.weight
-module.layer3.1.bn1.bias    
-module.layer3.1.conv2.weight
-module.layer3.1.bn2.weight
-module.layer3.1.bn2.bias           
-module.layer3.1.conv3.weight       
-module.layer3.1.bn3.weight       
-module.layer3.1.bn3.bias    
-module.layer3.2.conv1.weight
-module.layer3.2.bn1.weight
-module.layer3.2.bn1.bias    
-module.layer3.2.conv2.weight
-module.layer3.2.bn2.weight
-module.layer3.2.bn2.bias    
-module.layer3.2.conv3.weight
-module.layer3.2.bn3.weight
-module.layer3.2.bn3.bias    
-module.layer3.3.conv1.weight
-module.layer3.3.bn1.weight
-module.layer3.3.bn1.bias    
-module.layer3.3.conv2.weight
-module.layer3.3.bn2.weight
-module.layer3.3.bn2.bias    
-module.layer3.3.conv3.weight
-module.layer3.3.bn3.weight
-module.layer3.3.bn3.bias
-module.layer3.4.conv1.weight
-module.layer3.4.bn1.weight
-module.layer3.4.bn1.bias
-module.layer3.4.conv2.weight
-module.layer3.4.bn2.weight
-module.layer3.4.bn2.bias  
-module.layer3.4.conv3.weight
-module.layer3.4.bn3.weight         
-module.layer3.4.bn3.bias 
-module.layer3.5.conv1.weight
-module.layer3.5.bn1.weight
-module.layer3.5.bn1.bias
-module.layer3.5.conv2.weight
-module.layer3.5.bn2.weight
-module.layer3.5.bn2.bias
-module.layer3.5.conv3.weight
-module.layer3.5.bn3.weight
-module.layer3.5.bn3.bias
-module.layer4.0.conv1.weight
-module.layer4.0.bn1.weight
-module.layer4.0.bn1.bias
-module.layer4.0.conv2.weight
-module.layer4.0.bn2.weight
-module.layer4.0.bn2.bias
-module.layer4.0.conv3.weight
-module.layer4.0.bn3.weight
-module.layer4.0.bn3.bias
-module.layer4.0.downsample.0.weight
-module.layer4.0.downsample.1.weight
-module.layer4.0.downsample.1.bias
-module.layer4.1.conv1.weight
-module.layer4.1.bn1.weight
-module.layer4.1.bn1.bias
-module.layer4.1.conv2.weight
-module.layer4.1.bn2.weight
-module.layer4.1.bn2.bias
-module.layer4.1.conv3.weight
-module.layer4.1.bn3.weight
-module.layer4.1.bn3.bias
-module.layer4.2.conv1.weight
-module.layer4.2.bn1.weight
-module.layer4.2.bn1.bias
-module.layer4.2.conv2.weight
-module.layer4.2.bn2.weight
-module.layer4.2.bn2.bias
-module.layer4.2.conv3.weight
-module.layer4.2.bn3.weight
-module.layer4.2.bn3.bias
-module.fc.weight
-module.fc.bias
-module.projection.0.weight
-module.projection.0.bias
-module.projection.2.weight
-module.projection.2.bias
-'''
+        self.gate = nn.Sequential(
+            nn.Linear(self.feat_dim, 256), nn.ReLU(),
+            nn.Linear(256, 128), nn.ReLU(),
+            nn.Linear(128, 3)
+        )
+
+        self.bias_net = DynamicBiasNetwork(num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, norm, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, planes, norm, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def compute_dcb_loss(self, device):
+        """
+        """
+        if self.class_probs is None:
+            return torch.tensor(0.0).to(device)
+
+        all_classes = torch.arange(self.num_classes).to(device)
+        # 获取所有类别的 Bias, 并取3个专家的均值作为该类的 scalar bias
+        all_biases = self.bias_net(all_classes)
+        avg_bias = all_biases.mean(dim=1)
+
+        class_probs = self.class_probs.to(device)
+
+        prob_bias = torch.sigmoid(avg_bias)
+        dcb_loss = torch.sum(class_probs * torch.log(prob_bias + 1e-6))
+
+        return dcb_loss
+
+    def forward_features(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        h4 = self.layer4(out)
+        p4 = self.pooling(h4)
+        p4 = p4.view(p4.size(0), -1)
+        return p4, h4
+
+    def forward(self, x, class_idx=None):
+        p4, h4 = self.forward_features(x)
+        device = x.device
+
+        # --- 1. Bias-aware Classification (对应文本 Eq. 1 的第一部分) ---
+        logits = self.classifier(p4)
+
+        if self.training:
+            all_classes = torch.arange(self.num_classes).to(device)
+            all_biases = self.bias_net(all_classes).mean(dim=1)
+            logits = logits + self.gamma * all_biases
+
+        gate_logits = self.gate(p4)
+
+        rec0 = self.decoder0(h4)
+        rec1 = self.decoder1(h4)
+        rec2 = self.decoder2(h4)
+
+        loss_rec0 = F.mse_loss(rec0, x, reduction='none').mean(dim=[1, 2, 3])
+        loss_rec1 = F.mse_loss(rec1, x, reduction='none').mean(dim=[1, 2, 3])
+        loss_rec2 = F.mse_loss(rec2, x, reduction='none').mean(dim=[1, 2, 3])
+
+        # Quality Score
+        q0 = -torch.log(loss_rec0 + 1e-8)
+        q1 = -torch.log(loss_rec1 + 1e-8)
+        q2 = -torch.log(loss_rec2 + 1e-8)
+        quality_logits = torch.stack([q0, q1, q2], dim=1)
+
+        # Routing Bias
+        if class_idx is not None:
+            bias_logits = self.bias_net(class_idx)
+        else:
+            bias_logits = torch.zeros_like(gate_logits)
+
+        final_gate_logits = (self.alpha * gate_logits +
+                             self.beta * quality_logits +
+                             self.gamma * bias_logits)
+
+        weights = F.softmax(final_gate_logits, dim=1)
+
+        w_expanded = weights.unsqueeze(2).unsqueeze(3).unsqueeze(4)
+        recs = torch.stack([rec0, rec1, rec2], dim=1)
+        rec_final = (w_expanded * recs).sum(dim=1)
+
+        recon_loss = F.mse_loss(rec_final, x) * self.recon_weight
+
+        mean_usage = weights.mean(dim=0)
+        loss_balance = F.mse_loss(mean_usage, torch.ones(3).to(device) / 3)
+        loss_sparse = -torch.sum(weights * torch.log(weights + 1e-8), dim=1).mean()
+        gate_loss = loss_balance + self.sparse_weight * loss_sparse
+
+        dcb_reg_loss = self.compute_dcb_loss(device) * self.dcb_weight
+
+        return logits, recon_loss, gate_loss, dcb_reg_loss
+        
+def ResNet18AE(num_classes, class_freq, **kwargs):
+    return ResNetAE(BasicBlock, [2, 2, 2, 2], num_classes=num_classes, class_freq=class_freq, **kwargs)
+
+
+def ResNet34AE(num_classes, class_freq, **kwargs):
+    return ResNetAE(BasicBlock, [3, 4, 6, 3], num_classes=num_classes, class_freq=class_freq, **kwargs)
