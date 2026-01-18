3bbec35c0111ae8c1670e1fbbd34debe7a7210c5
emomakeroO <128972902+emomakeroO@users.noreply.github.com>
2026-01-18 13:38:52 +0800

    Update train.py

--- train.py
+++ train.py
@@ -11,8 +11,8 @@ from models.our_resnet_imagenet import ResNet50
 from datasets.ImbalanceCIFAR import IMBALANCECIFAR10, IMBALANCECIFAR100
 from datasets.ImbalanceImageNet import LT_Dataset
 from datasets.tinyimages_300k import TinyImages
-from models.our_resnete_db import ResNet18AE, ResNet34AE
-from models.our_resnet_imagenet_db import ResNet50AE
+from models.our_resnete import ResNet18AE, ResNet34AE
+from models.our_resnet_imagenet import ResNet50AE
 
 from utils.utils import *
 from utils.ltr_metrics import *
